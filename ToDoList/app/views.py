0900fc6619947b03e74330d9f5dbd5ef4513a905
Hyeyoung-Eum <djagpdud125@korea.ac.kr>
2023-06-04 07:20:09 +0900

    230604 version 기록 : D-Day 기능 및 main 기본 디자인 완성

--- ToDoList/app/views.py
+++ ToDoList/app/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render, redirect
 from .models import Todolist
-from datetime import datetime
+from datetime import datetime, timedelta, date
 # Create your views here.
 def main(request):
     todolists = Todolist.objects.all()
-    return render(request, 'main.html', {'todolists':todolists})
+    today=date.today()
+    return render(request, 'main.html', {'todolists':todolists, 'today':today})
 
 def new(request):
-    
     if request.method =='POST':
+        #due가 자꾸 값이 비어있으면 오류가 나서,기본값으로 1주일의 시간을 주기로 했음
+        if request.POST['due']=="":
+            w=timedelta(weeks=1)
+            due_result=datetime.now()+w
+        else :
+            due_result=request.POST['due']
         new_todolist=Todolist.objects.create(
             title=request.POST['title'],
             content=request.POST['content'],
             status=request.POST['status'],
-            due=request.POST['due'],
-            create_at=datetime.now()
+            due=due_result,
+            created_at=datetime.now()
         )
 
         return redirect('detail', new_todolist.id)
@@ -28,11 +34,17 @@ def update(request, todolist_id):
     todolist=Todolist.objects.get(id=todolist_id)
 
     if request.method =='POST':
+        #due가 자꾸 값이 비어있으면 오류가 나서,기본값으로 1주일의 시간을 주기로 했음
+        if request.POST['due']=="":
+            w=timedelta(weeks=1)
+            due_result=datetime.now()+w
+        else :
+            due_result=request.POST['due']
         Todolist.objects.filter(id=todolist_id).update(
                 title=request.POST['title'],
                 content=request.POST['content'],
                 status=request.POST['status'],
-                due=request.POST['due'],
+                due=due_result,
                 rewrited_at=datetime.now()
 
         )
